7d8d9bd37df4ae1d3b3b288b3077720f6f567214
maxfraid <ziga_max_zaga@mail.ru>
2024-02-04 17:43:51 +0300

    fixed metrics.py

--- common/metrics.py
+++ common/metrics.py
@@ -1,32 +1,30 @@
 import os
 import numpy as np
 from PIL import Image
-from utils import scan_directory, get_time
+from utils import get_time
 
 
 @get_time
-def pix2pix(current_image_path: str, second_image_path: str) -> bool:
-    """## Функция для сравнения двух изображений методом Pixel to Pixel.
+def pix2pix(first_image_path: str, second_image_path: str) -> bool:
+    """
+    ## Функция для сравнения двух изображений методом Pixel to Pixel.
 
     ### Input:
-    current_image_path (string): path to the first image
-    second_image_path: str
+    first_image_path (string): path to the first image
+    second_image_path: (string): path to the second image
 
     ### Output:
-    - image equal dict to current_image_path (boolean)
-    example of output: {image_name, True/False}
+    - image equal (boolean)
     """
-    image_pathlist = scan_directory(dataset_path)
 
-    duplicates = {}
+    resized_image1 = Image.open(first_image_path).resize((512, 512))
+    resized_image2 = Image.open(second_image_path).resize((512, 512))
 
-    resized_image1 = Image.open(current_image_path).resize((512, 512))
+    are_equal = np.array_equal(np.array(resized_image1), np.array(resized_image2))
 
-    for image in image_pathlist:
-        image_name = os.path.basename(image)
-        second_image = Image.open(image)
-        resized_image2 = np.array(second_image.resize((512, 512)))
+    return are_equal
 
-        are_equal = np.array_equal(np.array(resized_image1), np.array(resized_image2))
 
-    return duplicates
+if __name__ == "__main__":
+    print(pix2pix("dataset/000.jpg", "dataset/000.jpg"))
+    print(pix2pix("dataset/000.jpg", "dataset/001.jpg"))
