fb16d0a6c38f880aa854cd87bcce5608d2cf3e4f
Reape4er <ivan.artamonov03@yandex.ru>
2024-02-18 16:07:52 +0300

    metric result to csv

--- common/metrics.py
+++ common/metrics.py
@@ -1,7 +1,9 @@
 from itertools import product
 from matplotlib import pyplot as plt
 import numpy as np
+from numpy import array_equal
 from PIL import Image
+import pandas as pd
 from utils import get_time, color_print
 from sklearn.metrics import mean_squared_error as mse_sklearn
 from skimage.metrics import normalized_root_mse as nrmse_skimage
@@ -14,24 +16,6 @@ from concurrent.futures import ThreadPoolExecutor
 # ==================================================================================================================================
 # |                                                              METRICS                                                           |
 # ==================================================================================================================================
-
-
-def pix2pix_np(image1: np.array, image2: np.array) -> bool:
-    """
-    Функция для сравнения двух изображений методом Pixel to Pixel.
-
-    Вход:
-    - image1 (ArrayLike): одномерный массив первого изображения
-    - image2 (ArrayLike): одномерный массив второго изображения
-
-    Вывод:
-    - image_equal (boolean): True, если изображения идентичны, иначе False
-    """
-
-    are_equal = np.array_equal(image1, image2)
-
-    return are_equal
-
 
 class Metric:
     def __init__(self, image_paths1: list, image_paths2: list):
@@ -59,6 +43,7 @@ class Metric:
         with ThreadPoolExecutor() as executor:
             resized_images = list(executor.map(self.load_and_resize_image, image_paths))
             # print(len(resized_images))
+
         return resized_images
 
     def load_and_resize_image(self, image_path: str) -> np.array:
@@ -76,9 +61,10 @@ class Metric:
             img_resized = img.resize((512, 512))
             img_array = np.array(img_resized)
             # print(image_path)
+
         return img_array.flatten()
 
-    def calculate_metric(self, metric_function: function) -> list[float]:
+    def calculate_metric(self, metric_function: object, save_to_csv) -> list[float]:
         """
         Функция для сравнения по выбранной метрике
 
@@ -89,7 +75,6 @@ class Metric:
         """
 
         metric_values = []
-        k = 0
 
         with ThreadPoolExecutor() as executor:
             for img1 in self.resized_images1:
@@ -98,38 +83,43 @@ class Metric:
                         lambda img2: metric_function(img1, img2), self.resized_images2
                     )
                 )
-                k += 1
-                print(k)
                 metric_values.append(row)
+        if save_to_csv == True:
+            self.save(metric_function.__name__, metric_values)
         return metric_values
 
-    def pix2pix(self) -> list:
-        return self.calculate_metric(pix2pix_np)
+    def pix2pix(self, save_to_csv=False) -> list:
+        return self.calculate_metric(array_equal, save_to_csv)
 
-    def mae(self) -> list:
-        return self.calculate_metric(mae_skimage)
+    def mae(self, save_to_csv=False) -> list:
+        return self.calculate_metric(mae_skimage, save_to_csv)
 
-    def mse(self) -> list:
-        return self.calculate_metric(mse_sklearn)
+    def mse(self, save_to_csv=False) -> list:
+        return self.calculate_metric(mse_sklearn, save_to_csv)
 
-    def nrmse(self) -> list:
-        return self.calculate_metric(nrmse_skimage)
+    def nrmse(self, save_to_csv=False) -> list:
+        return self.calculate_metric(nrmse_skimage, save_to_csv)
 
-    def ssim(self) -> list:
+    def ssim(self, save_to_csv=False) -> list:
         return self.calculate_metric(
-            lambda x, y: ssim_skimage(x, y, win_size=3)
+            lambda x, y: ssim_skimage(x, y, win_size=3), save_to_csv
         )  # < ================   WTF IS THIS
 
-    def psnr(self) -> list:
-        return self.calculate_metric(psnr_skimage)
+    def psnr(self, save_to_csv=False) -> list:
+        return self.calculate_metric(psnr_skimage, save_to_csv)
 
-    def nmi(self) -> list:
-        return self.calculate_metric(nmi_skimage)
+    def nmi(self, save_to_csv) -> list:
+        return self.calculate_metric(nmi_skimage, save_to_csv)
 
     def show(self, matrix):
         plt.imshow(matrix, cmap="viridis", interpolation="nearest")
         plt.colorbar()
         plt.show()
+    def save(self, metric_name: str, metric_values):
+        df = pd.DataFrame(metric_values, columns = self.image_paths2, index = self.image_paths1)
+        csv_filename = f'{metric_name}.csv'
+        df.to_csv(csv_filename)
+        print(f"CSV файл '{csv_filename}' успешно создан.")
 
 
 # ==================================================================================================================================
@@ -137,27 +127,28 @@ class Metric:
 if __name__ == "__main__":
     print("starting test...")
 
-    # image_paths1 = ["test_images/PSNR-base.jpg", "test_images/PSNR-90.jpg", "test_images/PSNR-30.jpg", "test_images/PSNR-10.jpg"]
-    # image_paths2 = ["test_images/PSNR-base.jpg", "test_images/PSNR-90.jpg", "test_images/PSNR-30.jpg", "test_images/PSNR-10.jpg"]
-    # from utils import scan_directory
-    # import os
+    #image_paths1 = ["test_images/PSNR-base.jpg", "test_images/PSNR-90.jpg", "test_images/PSNR-30.jpg", "test_images/PSNR-10.jpg"]
+    #image_paths2 = ["test_images/PSNR-base.jpg", "test_images/PSNR-90.jpg", "test_images/PSNR-30.jpg", "test_images/PSNR-10.jpg"]
+    
+    from utils import scan_directory
+    import os
 
-    # image_paths1 = scan_directory("dataset")
-    # x2 = list(map(lambda x: os.path.join(x[0], x[1]), image_paths1))
-    # print()
+    image_paths3 = scan_directory("dataset")
+    x2 = list(map(lambda x: os.path.join(x[0], x[1]), image_paths3))
+    print()
 
-    # metric = Metric(x2, x2)
+    metric = Metric(x2, x2)
 
-    # pix2pix_values = metric.pix2pix()
-    # mae_values = metric.mae()
+    #pix2pix_values = metric.pix2pix(True)
+    mae_values = metric.mae(True)
     # mse_values = metric.mse()
     # nrmse_values = metric.nrmse()
     # ssim_values = metric.ssim()
     # psnr_values = metric.psnr()
     # nmi_values = metric.nmi()
 
-    # print(f"pix2pix values: {pix2pix_values}")
-    # print(f"mae values: {mae_values}")
+    #print(f"pix2pix values: {pix2pix_values}")
+    print(f"mae values: {mae_values}")
     # print(f"MSE values: {mse_values}")
     # print(f"NRMSE values: {nrmse_values}")
     # print(f"SSIM values: {ssim_values}")
@@ -165,7 +156,7 @@ if __name__ == "__main__":
     # print(f"NMI values: {nmi_values}")
 
     # metric.show(pix2pix_values)
-    # metric.show(mae_values)
+    metric.show(mae_values)
     # metric.show(mse_values)
     # metric.show(nrmse_values)
     # metric.show(ssim_values)
