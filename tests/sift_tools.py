965c9c3adc6ab6b7a783e6d44c4b2ef2b65e08b6
cls <48602112+maxfraid@users.noreply.github.com>
2024-02-23 17:36:45 +0300

    + sift_tools.py

--- tests/sift_tools.py
+++ tests/sift_tools.py
@@ -1,63 +1,122 @@
+import os
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from ipywidgets import interact
 from scipy import stats
-from common.utils import scan_directory
+from common.utils import color_print, scan_directory
 
 
-def num2fixstr(x, d):
+# ==================================================================================================================================
+# |                                                             SIFT TOOLS                                                         |
+# ==================================================================================================================================
+
+
+def load_image(image_path: str, echo: bool = False) -> np.ndarray:
     """
-    Преобразует число в строку заданной длины, добавляя нули слева.
+    Загружает изображение из файла и масштабирует его до ширины 512 пикселей, сохраняя пропорции.
 
     Parameters:
-        - x (int): число, которое нужно преобразовать в строку.
-        - d (int): длина строки, которую нужно вернуть.
+        - image_path (str): Путь к файлу изображения.
+        - echo (str, optional): Управляет выводом сообщения о загрузке изображения. Если "on", то выводится сообщение.
 
     Returns:
-        - str: строка заданной длины.
+        - numpy.ndarray: Загруженное и масштабированное изображение в формате RGB.
     """
 
-    st = "%0*d" % (d, x)
+    if echo == True:
+        color_print("create", "create", f"Loading image {image_path}", True)
 
-    return st
+    # Загрузка изображения и преобразование его в формат RGB
+    img = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+
+    # Получение текущих размеров изображения
+    height, width = img.shape[:2]
+
+    # Вычисление новых размеров, сохраняя пропорции
+    new_width = 512
+    new_height = int(height * (new_width / width))
+
+    # Масштабирование изображения до новых размеров
+    resized_img = cv2.resize(img, (new_width, new_height))
+
+    return resized_img
 
 
-def ImageLoad(prefix, num_img, echo="off"):
+# ==================================================================================================================================
+
+
+def get_descriptors(dataset_path: str, echo: bool = False):
     """
-    Загружает изображение из файла.
+    Извлекает дескрипторы SIFT из изображений в указанной директории.
 
     Parameters:
-        - prefix (str): префикс имени файла изображения.
-        - num_img (int): номер изображения.
-        - echo (str, опционально): флаг, указывающий, нужно ли выводить информацию о загрузке изображения. По умолчанию выключен.
+        - images_dir (str): Путь к директории с изображениями.
 
     Returns:
-        - numpy.ndarray: загруженное изображение в формате RGB.
+        - numpy.ndarray: Массив дескрипторов SIFT.
+        - numpy.ndarray: Массив меток изображений.
     """
 
-    st = prefix + num2fixstr(num_img, 5) + ".png"
+    feature_extractor = cv2.SIFT_create()
 
-    if echo == "on":
-        print("loading image " + st + "...")
+    t = 0
+    Xt = np.zeros((50000, 128))
+    yt = np.zeros((50000,))
 
-    img = cv2.cvtColor(cv2.imread(st), cv2.COLOR_BGR2RGB)
+    image_paths = scan_directory(dataset_path)
+    image_full_paths = list(map(lambda x: os.path.join(x[0], x[1]), image_paths))
 
-    return img
+    image_count = len(image_full_paths)
+
+    print(image_full_paths) if echo else None
+
+    for i in range(image_count):
+        image = load_image(image_full_paths[i], echo)
+        J = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        # SIFT extraction
+        kp, desc = feature_extractor.detectAndCompute(J, None)
+        ni = desc.shape[0]
+        for j in range(ni):
+            f = desc[j, :]
+            Xt[t, :] = f / np.linalg.norm(f)
+            yt[t] = i
+            t = t + 1
+
+        if echo:
+            color_print("done", "done", f"{str(ni)} Дескрипторы были извлечены.")
+
+    X = Xt[0:t, :]
+    y = yt[0:t]
+
+    if echo:
+        color_print(
+            "done",
+            "done",
+            f"Количество дескрипторов SIFT в {str(image_count)} images: {str(t)}",
+            True,
+        )
+
+    return X, y
+
+
+# ==================================================================================================================================
 
 
 def find_similar_image(itest, X, y):
     """
-    Определяет наиболее похожее изображение на заданное изображение.
+    Находит наиболее похожее изображение на тестовом изображении с помощью дескрипторов SIFT.
 
     Parameters:
-        - itest (int): индекс тестируемого изображения.
-        - X (numpy.ndarray): матрица признаков изображений.
-        - y (numpy.ndarray): метки классов изображений.
+        itest (int): Индекс тестового изображения.
+        X (numpy.ndarray): Массив дескрипторов SIFT.
+        y (numpy.ndarray): Массив меток изображений.
 
     Returns:
-        - int: индекс наиболее похожего изображения.
+        int: Индекс наиболее похожего изображения.
     """
-    ik = itest - 1
+
+    ik = itest
     ii = np.where(y == ik)[0]
     jj = np.where(y != ik)[0]
 
@@ -82,28 +141,31 @@ def find_similar_image(itest, X, y):
     kk = np.where(z > 0.9)
 
     m = stats.mode(d[kk])
-    ifound = int(m[0]) + 1
+    ifound = int(m[0])
 
     return ifound
 
 
+# ==================================================================================================================================
+
+
 def visualize_similar_images(itest):
     """
-    Визуализирует тестовое изображение и его наиболее похожее изображение.
+    Визуализирует тестовое и наиболее похожее изображения.
 
     Parameters:
-        - itest (int): индекс тестируемого изображения.
+        itest (int): Индекс тестового изображения.
+        images_fpath (list): Список путей к изображениям.
     """
 
     ifound = find_similar_image(itest, X, y)
-    I = ImageLoad("cows_dataset/V", itest, echo="on")
-
+    image = load_image(image_full_paths[itest], True)
     print("Тестовое изображение: " + str(itest))
-    plt.imshow(I, cmap="gray")
+    plt.imshow(image, cmap="gray")
     plt.axis("off")
     plt.show()
     print(" ")
-    J = ImageLoad("cows_dataset/V", ifound, echo="on")
+    J = load_image(image_full_paths[ifound], True)
     print(
         "Для изображения "
         + str(itest)
@@ -117,6 +179,10 @@ def visualize_similar_images(itest):
     print("--------------------------------------------------")
 
 
+# Пример использования с ipywidgets interact
+interact(visualize_similar_images, itest=(0, 2))
+
+# ==================================================================================================================================
+
 if __name__ == "__main__":
-    print(num2fixstr(3, 5))
-    print(scan_directory("tests/cows_dataset/"))
+    print("gdgd")
